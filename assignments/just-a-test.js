7d009b5535bfa1dcfd5670de16cb8ab0345dab18
Neil on School Computer <50129064+mccrackenn@users.noreply.github.com>
2019-10-28 12:20:03 -0700

    Javascript learning from school

---
+++ assignments/just-a-test.js
@@ -0,0 +1,19 @@
+const city="New York City";
+function logCitySkyline(){
+    let skyscraper="Empire State Building";
+    return 'The stars over the'+ skyscraper+ 'in'+city;
+};
+
+satellite='The Moon';
+galaxy='The Milky Way';
+stars='North Star';
+
+const callMyNightSky=()=>{
+    return 'Night Sky: '+satellite + ','+stars + ', and' + galaxy;
+}
+
+const logVisibleLightWaves=()=> {
+    const lightWaves='Moonlight';
+     console.log(lightWaves);
+}
+logVisibleLightWaves();
---
+++ javascript-tests/primitives.js
@@ -0,0 +1,28 @@
+const myString="Learning Javascript in Class";
+const myBoolean=true;
+const myNull=null;
+let myUndefined; 
+
+myUndefined="Learning more Java script in class"; 
+
+const myFirstNumericVariable=3;
+
+var oldStyle="This is the way we used to do it";
+let newChangeableStyly="this allows you to declare a variable that can be changed.";
+const constantVariable="This variable cannot be changed. Except...";
+
+const quotable='He said, "Hello". that\'s all folks'
+
+console.log(myString);
+console.log(typeof(myString));
+console.log(myFirstNumericVariable);
+console.log(myBoolean);
+console.log('type', typeof myFirstNumericVariable);
+
+console.log(myNull);
+console.log('type', typeof myNull);
+
+console.log(myUndefined);
+console.log('type', typeof myUndefined);
+console.log('add 3 to 25',myFirstNumericVariable + 25);
+console.log('Add undefined',myUndefined+25);
