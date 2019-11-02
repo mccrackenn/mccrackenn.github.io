3362eadbb3d38dcf1fd6853f258460ac5fa5a867
Neil McCracken <50129064+mccrackenn@users.noreply.github.com>
2019-11-02 12:19:50 -0700

    Changes to javascript branching

--- javascript-tests/branching.js
+++ javascript-tests/branching.js
@@ -11,7 +11,7 @@ if (height >= 25) {
     console.log('We have a little one');
 }
 
-height = 5;
+height = -5;
 if (height >= 25) {
     console.log('We have a big one');
 } else if (height >= 0) {
@@ -39,8 +39,8 @@ window.onload = () => {
     const simpleBranchDisplay = document.getElementById('simpleBranchDisplay');
     
     simpleBranchAction.onclick = (event) => {
-        let height = 4;
-        if (height >= 5) {
+        let height = 50;
+        if (height >= 25) {
             simpleBranchDisplay.textContent = 'We have a big one';
         } else {
             simpleBranchDisplay.textContent = 'We have a little one';
