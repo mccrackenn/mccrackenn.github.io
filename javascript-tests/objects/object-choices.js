e1ee0491db69df3dfeab75b3c3146357b72f8a85
Neil on School Computer <50129064+mccrackenn@users.noreply.github.com>
2019-11-20 12:20:28 -0800

    Changes from class to objects

--- javascript-tests/objects/object-choices.js
+++ javascript-tests/objects/object-choices.js
@@ -1,29 +1,57 @@
-const simpleObject={
-    sayName:()=> {
-        console.log('simple Object');
+window.onload = () => {
+
+    const simpleObjectDisplay = document.getElementById('simpleObjectDisplay');
+    const functionObjectDisplay = document.getElementById('functionObjectDisplay');
+    const customClassDisplay = document.getElementById('customClassDisplay');
+
+    //simpleObjectDisplay.textContent="foo";
+    // function loader() {
+    //     const simpleObjectAction = document.getElementById('simpleObjectAction');
+    //     const functionObjectAction = document.getElementById('functionObjectAction');
+    //     const customClassAction = document.getElementById('customClassAction')
+    // }
+
+
+
+
+
+    const simpleObject = {
+        firstName: "James",
+        lastName: "Watson",
+        sayName: function () {
+            const fullName= this.firstName + " " + this.lastName;
+            console.log(fullName);
+            //simpleObjectDisplay.textContent=fullName;
+        }
+    };
+    simpleObjectAction.onclick = function() {
+        simpleObject.sayName();
+        simpleObjectDisplay.textContent=fullName;
     }
-};
 
-simpleObject.dynamicMethod=()=>{
-    console.log("Dynamic Method");
-}
-
-function FunctionObject(){
-    FunctionObject.prototype.sayName=function() {
-        console.log("Function Object");
+    simpleObject.dynamicMethod = () => {
+        console.log("Dynamic Method");
     }
-}
 
-class CustomClass{
-    sayName() {
-        console.log("CustomClass");
+    function FunctionObject() {
+        FunctionObject.prototype.sayName = function () {
+            console.log("Function Object");
+        }
     }
+
+    class CustomClass {
+        sayName() {
+            console.log("CustomClass");
+        }
+    }
+
+    simpleObject.sayName();
+    simpleObject.dynamicMethod();
+    //Need to call new on it, constructor 
+    const functionObject = new FunctionObject();
+    functionObject.sayName();
+
+    const custClass = new CustomClass();
+    custClass.sayName();
+
 }
-
-simpleObject.sayName();
-simpleObject.dynamicMethod();
-const functionObject=new FunctionObject();
-functionObject.sayName();
-
-const custClass=new CustomClass();
-customClass.sayName();
